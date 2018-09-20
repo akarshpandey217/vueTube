600425f1acecfad6de6e75bd0d02c66b346055c2
akarshpandey217 <37379312+akarshpandey217@users.noreply.github.com>
2018-09-20 13:14:43 +0530

    Create playerView.vue

---
+++ src/components/subComponents/bodyComponents/playerView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-card>
+            <y-player v-bind:videoData="videoData"></y-player>
+        </v-card>
+        <transition name="fade" mode="out-in">
+            <v-comments v-show="!videoStopped"></v-comments>
+        </transition>
+    </div>
+</template>
+
+<script>
+import yPlayer from "./yPlayer";
+import vComments from "./videoComments";
+export default {
+    props:['videoData','videoStopped'],
+    components:{
+        'y-player':yPlayer,
+        'v-comments' : vComments
+    }
+}
+</script>
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
